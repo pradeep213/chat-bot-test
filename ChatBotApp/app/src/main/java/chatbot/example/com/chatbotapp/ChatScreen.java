9fbc028285a2899eb50329409f7b487bbca50240
Pradeep Sakthi <pradeep.sakthi@redbus.com>
2018-01-21 10:51:49 +0530

    initial commit after setup

---
+++ ChatBotApp/app/src/main/java/chatbot/example/com/chatbotapp/ChatScreen.java
@@ -0,0 +1,35 @@
+package chatbot.example.com.chatbotapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pradeepsakthi on 21/01/18.
+ */
+
+public class ChatScreen extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    
+}
